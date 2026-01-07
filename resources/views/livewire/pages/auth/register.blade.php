7289f653e101072b2ba9f1721df3dfa8a627c7f4
Caesar <caesar22101994@gmail.com>
2026-01-07 11:58:32 +0700

    Add login multi user

--- resources/views/livewire/pages/auth/register.blade.php
+++ resources/views/livewire/pages/auth/register.blade.php
@@ -92,14 +92,20 @@ new #[Layout('layouts.guest')] class extends Component
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}" wire:navigate>
-                {{ __('Already registered?') }}
-            </a>
+        <div class="flex items-center justify-between mt-4">
+            <p class="text-xs text-gray-500">
+                {{ __('Pilih peran sesuai kebutuhan: Admin untuk pengelola, Customer untuk pembeli.') }}
+            </p>
 
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
+            <div class="flex items-center">
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}" wire:navigate>
+                    {{ __('Already registered?') }}
+                </a>
+
+                <x-primary-button class="ms-4">
+                    {{ __('Register') }}
+                </x-primary-button>
+            </div>
         </div>
     </form>
 </div>
